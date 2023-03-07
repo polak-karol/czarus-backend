9f7084bafef468370eed92af547fc851cedbabd8
Karol Polak <karolpolak987@gmail.com>
2023-03-07 12:37:05 +0100

    Refactoring

--- helpers/DateTimeHelper.py
+++ helpers/DateTimeHelper.py
@@ -5,12 +5,20 @@ class DateTimeHelper:
     current_date = datetime.datetime.today()
 
     @classmethod
-    def get_closest_date_of_weekday(cls, date, day):
+    def get_closest_date_of_weekday(cls, date: datetime.datetime, day: int) -> datetime.datetime:
+        """
+        Returns the closest day of provided weekday.
+        0 - Monday
+        6 - Sunday
+        """
         days = (day - date.weekday() + 7) % 7
         return date + datetime.timedelta(days=days)
 
     @classmethod
-    def is_date_in_current_week(cls, date):
+    def is_date_in_current_week(cls, date: datetime.datetime) -> bool:
+        """
+        Returns boolean if date is in current week.
+        """
         return (
             cls.get_closest_date_of_weekday(cls.current_date, 6)
             >= date
