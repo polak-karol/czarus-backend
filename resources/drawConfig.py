41fbc339ab355b85286643f5e017c9147181c823
polak-karol <karolpolak987@gmail.com>
2023-03-30 12:14:21 +0200

    Fix naming method issue

--- resources/drawConfig.py
+++ resources/drawConfig.py
@@ -10,9 +10,7 @@ draw_config_schema = DrawConfigSchema()
 class DrawConfig(BaseResource):
     @classmethod
     def get(cls, guild_id):
-        draw_config = DrawConfigModel.find_birthday_by_user_id(
-            guild_id, request.args["user_id"]
-        ).first()
+        draw_config = DrawConfigModel.find_draw_config_by_guild_id(guild_id).first()
 
         if not draw_config:
             return {"message": "Not found"}, 404
