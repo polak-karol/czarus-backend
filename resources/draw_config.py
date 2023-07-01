19c559f23a542eb8cfcdcf6005392505514b69b7
karol-polak <karolpolak987@gmail.com>
2023-07-01 19:20:52 +0200

    Add validation to draw_config resource

--- resources/draw_config.py
+++ resources/draw_config.py
@@ -1,4 +1,5 @@
 from flask import request
+from flask_jwt_extended import jwt_required
 
 from resources.base import BaseResource
 from schemas.draw_config import DrawConfigSchema
@@ -9,7 +10,11 @@ draw_config_schema = DrawConfigSchema()
 
 class DrawConfig(BaseResource):
     @classmethod
+    @jwt_required(optional=True)
     def get(cls, guild_id):
+        if not cls.is_client_authorized():
+            return cls.not_authorized_response
+
         draw_config = DrawConfigModel.find_draw_config_by_guild_id(guild_id).first()
 
         if not draw_config:
@@ -18,7 +23,11 @@ class DrawConfig(BaseResource):
         return {"data": draw_config_schema.dump(draw_config)}, 200
 
     @classmethod
+    @jwt_required(optional=True)
     def put(cls, guild_id):
+        if not cls.is_client_authorized():
+            return cls.not_authorized_response
+
         draw_config_json = request.get_json()
         draw_config_json["guildId"] = guild_id
         draw_config_query = DrawConfigModel.find_draw_config_by_guild_id(guild_id)
