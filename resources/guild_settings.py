85c4e79b24184a1e2cc8ce178bc9c20cf7f51495
karol-polak <karolpolak987@gmail.com>
2023-07-06 19:17:34 +0200

    Fix put method in GuildSettings resource

--- resources/guild_settings.py
+++ resources/guild_settings.py
@@ -11,18 +11,19 @@ guild_settings_schema = GuildSettingsSchema()
 class GuildSettings(BaseResource):
     @classmethod
     @jwt_required(optional=True)
-    def put(cls):
+    def put(cls, guild_id):
         if not cls.is_client_authorized():
             return cls.not_authorized_response
 
         guild_settings_json = request.get_json()
-        guild_settings = GuildSettingsModel.find_guild_settings(guild_settings_json["guild_id"]).first()
+        guild_settings_json["guild_id"] = guild_id
+        guild_settings = GuildSettingsModel.find_guild_settings(guild_id).first()
 
         guild_settings = guild_settings_schema.load(guild_settings_json)
 
         guild_settings.save_to_db()
 
-        return guild_settings_schema.dump(guild_settings), 200
+        return {"data": guild_settings_schema.dump(guild_settings)}, 200
 
     @classmethod
     @jwt_required(optional=True)
