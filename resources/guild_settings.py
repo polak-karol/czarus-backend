65d4769a31bd9ca0b82f916822f47a4721e1abf3
karol-polak <karolpolak987@gmail.com>
2023-07-06 18:48:28 +0200

    Add get method to guild_settings resource

--- resources/guild_settings.py
+++ resources/guild_settings.py
@@ -1,14 +1,14 @@
 from flask import request
-from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 
+from resources.base import BaseResource
 from schemas.guild_settings import GuildSettingsSchema
 from models.guild_settings import GuildSettingsModel
 
 guild_settings_schema = GuildSettingsSchema()
 
 
-class GuildSettings(Resource):
+class GuildSettings(BaseResource):
     @classmethod
     @jwt_required(optional=True)
     def put(cls):
@@ -23,3 +23,13 @@ class GuildSettings(Resource):
         guild_settings.save_to_db()
 
         return guild_settings_schema.dump(guild_settings), 200
+
+    @classmethod
+    @jwt_required(optional=True)
+    def get(cls, guild_id):
+        if not cls.is_client_authorized():
+            return cls.not_authorized_response
+
+        guild_settings = GuildSettingsModel.find_guild_settings(guild_id).first()
+
+        return {"data": guild_settings_schema.dump(guild_settings)}, 200
