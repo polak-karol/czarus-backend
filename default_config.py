f46122f83e8c32ed286fda5b132a87654bb4844a
polak-karol <karolpolak987@gmail.com>
2023-09-24 21:09:09 +0200

    Update default_config

--- default_config.py
+++ default_config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "postgresql://czarus:czarus@localhost:5432/czarus"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
