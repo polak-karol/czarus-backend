64c5601d7fc4854d0116eb5bca39518498426a2e
polak-karol <karolpolak987@gmail.com>
2023-11-01 18:26:06 +0100

    Add wishes to birthday_configs table

--- models/birthday_config.py
+++ models/birthday_config.py
@@ -1,4 +1,5 @@
 from db import db
+from sqlalchemy.dialects import postgresql
 from models.base import BaseModel
 
 
@@ -8,6 +9,8 @@ class BirthdayConfigModel(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     birthdays_announcement_channel_id = db.Column(db.String(300), nullable=True)
     birthdays_handle_channel_id = db.Column(db.String(300), nullable=True)
+    wishes_singular = db.Column(postgresql.ARRAY(db.String), nullable=True)
+    wishes_plural = db.Column(postgresql.ARRAY(db.String), nullable=True)
     guild_id = db.Column(db.String(300), nullable=False, unique=True)
 
     @classmethod
